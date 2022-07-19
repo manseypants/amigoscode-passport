2fcefc10ea553374c6b2f0fca6bd0284a2da0542
manseypants <ridinspinaz04@gmail.com>
2022-07-19 17:17:01 -0500

    minimal app - nothing exciting unfortunatly :(

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Passport ukPassport = new Passport("1234", "UK", LocalDate.of(2025,1,1));
+        Passport usPassport = new Passport("3412", "USA", LocalDate.of(2030,4,11));
+    }
+
+    static class Passport {
+        String number;
+        String country;
+        LocalDate expiryDate;
+
+        Passport(String number, String country, LocalDate expiryDate) {
+            this.number = number;
+            this.country = country;
+            this.expiryDate = expiryDate;
+        }
     }
 }
